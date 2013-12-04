e76b1f245425f3818a3c5ec7b50899898dd8e583
rbelouin <rodolphe.belouin@gmail.com>
2013-12-04 19:29:05 +0100

    Filter contacts that have birthday in n days.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,8 @@
+import Control.Monad
+
 import Data.List
 import Data.Time.Calendar
+import Data.Time.LocalTime
 
 data Contact = Contact {
   id :: String,
@@ -8,8 +11,27 @@ data Contact = Contact {
   birthDate :: Day
 } deriving Show
 
+getZonedDay :: IO Day
+getZonedDay = fmap (localDay . zonedTimeToLocalTime) getZonedTime 
+
+isInNDays :: Integer -> Day -> IO Bool
+isInNDays n day = fmap (elem day) nDays
+  where nDays = fmap (\today -> [today..(addDays n today)]) getZonedDay
+
+nextBirthday :: Contact -> IO Day
+nextBirthday contact = nextDate (birthDate contact)
+  where
+    addYear   day = addGregorianYearsClip 1 day
+    nextDate  day = getZonedDay >>= (\today -> if day < today then (nextDate $ addYear day) else return day)
+
+hasBirthdayInNDays :: Integer -> Contact -> IO Bool
+hasBirthdayInNDays n contact = (nextBirthday contact) >>= (isInNDays n)
+
 contacts :: [Contact]
-contacts = [Contact "1" "JAMAL" "Ahmad" (fromGregorian 1930 7 2), Contact "2" "ROMANO" "Aldo" (fromGregorian 1941 1 16)]
+contacts = [Contact "1" "JAMAL" "Ahmad" (fromGregorian 1930 7 2), Contact "2" "ROMANO" "Aldo" (fromGregorian 1941 1 16), Contact "3" "BRUBECK" "Dave" (fromGregorian 1920 12 6)]
+
+contactsThatHaveBirthdayInNDays :: Integer -> IO [Contact]
+contactsThatHaveBirthdayInNDays n = (filterM (hasBirthdayInNDays n) contacts)
 
 main :: IO ()
-main = print contacts
+main = (contactsThatHaveBirthdayInNDays 8) >>= print
