efe919f10d058b87b63646a60f10e22b591993e6
rbelouin <rodolphe.belouin@gmail.com>
2013-12-05 10:35:23 +0100

    Contacts are stored in PostgreSQL.

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,15 @@
 import Control.Monad
 
+import Data.ByteString.Char8
 import Data.List
+import Data.Maybe
 import Data.Time.Calendar
 import Data.Time.LocalTime
 
 import Birthdays.Time
+
+import Database.HDBC
+import Database.HDBC.PostgreSQL
 
 data Contact = Contact {
   id :: String,
@@ -22,11 +27,27 @@ nextBirthday contact = nextDate (birthDate contact)
 hasBirthdayInNDays :: Integer -> Contact -> IO Bool
 hasBirthdayInNDays n contact = (nextBirthday contact) >>= (isInNDays n)
 
-contacts :: [Contact]
-contacts = [Contact "1" "JAMAL" "Ahmad" (fromGregorian 1930 7 2), Contact "2" "ROMANO" "Aldo" (fromGregorian 1941 1 16), Contact "3" "BRUBECK" "Dave" (fromGregorian 1920 12 6)]
+staticContacts :: [Contact]
+staticContacts = [Contact "1" "JAMAL" "Ahmad" (fromGregorian 1930 7 2), Contact "2" "ROMANO" "Aldo" (fromGregorian 1941 1 16), Contact "3" "BRUBECK" "Dave" (fromGregorian 1920 12 6)]
 
-contactsThatHaveBirthdayInNDays :: Integer -> IO [Contact]
-contactsThatHaveBirthdayInNDays n = (filterM (hasBirthdayInNDays n) contacts)
+contactsThatHaveBirthdayInNDays :: Integer -> [Contact] -> IO [Contact]
+contactsThatHaveBirthdayInNDays n contacts = (filterM (hasBirthdayInNDays n) contacts)
+
+fromPgcontact :: [SqlValue] -> Maybe Contact
+fromPgcontact [SqlByteString id, SqlByteString lastName, SqlByteString firstName, SqlLocalDate birthDate] = Just $ Contact (unpack id) (unpack lastName) (unpack firstName) birthDate
+fromPgcontact _ = Nothing
+
+fromPgcontacts :: [[SqlValue]] -> [Contact]
+fromPgcontacts = catMaybes . (fmap fromPgcontact)
+
+pgcontacts :: IO [Contact]
+pgcontacts = do
+  conn    <- connectPostgreSQL "dbname=birthdays"
+  result  <- quickQuery conn "SELECT * FROM contacts" []
+  --TODO: disconnect and keep result anywhere else
+  --disc    <- disconnect conn
+  return (fromPgcontacts result)
+
 
 main :: IO ()
-main = (contactsThatHaveBirthdayInNDays 8) >>= print
+main = pgcontacts >>= (contactsThatHaveBirthdayInNDays 8) >>= print
